533a10ff20b57b1b84be280f3dc5ed21cb218333
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-04-07 12:31:25 -0400

    OQL-16 code JDBC support

--- shared/src/main/scala/com/vinctus/oql2/JDBCOQLDataSource.scala
+++ shared/src/main/scala/com/vinctus/oql2/JDBCOQLDataSource.scala
@@ -1,6 +1,6 @@
 package com.vinctus.oql2
 
-abstract class JDBCOQLDataSource(driver: String) extends OQLDataSource {
+abstract class JDBCOQLDataSource(driver: String) extends SQLOQLDataSource {
 
   try Class.forName(driver)
   catch { case e: ClassNotFoundException => sys.error(e.getMessage) }
@@ -10,44 +10,5 @@ abstract class JDBCOQLDataSource(driver: String) extends OQLDataSource {
   def user: String
 
   def password: String
-
-  def mapType(typ: TypeSpecifier): String
-
-  def mapPKType(typ: PrimitiveType): String
-
-  def create(model: DataModel): Unit = {
-    val conn = connect
-
-    schema(model) foreach conn.execute
-    conn.close()
-  }
-
-  def schema(model: DataModel): Seq[String] = {
-    val tables =
-      for (entity <- model.entities.values.toList.sortBy(_.table))
-        yield {
-          val columns =
-            for (attribute <- entity.attributes.values)
-              yield
-                if (attribute.pk)
-                  s"  ${attribute.column} ${mapPKType(attribute.typ.asInstanceOf[PrimitiveType])} PRIMARY KEY"
-                else
-                  s"  ${attribute.column} ${mapType(attribute.typ)}${if (attribute.required) " NOT NULL" else ""}"
-
-          s"""
-           |CREATE TABLE ${entity.table} (
-           |${columns mkString ",\n"}
-           |);""".trim.stripMargin
-        }
-
-    val foreignKeys =
-      for (entity <- model.entities.values.toList.sortBy(_.table))
-        yield
-          for (attribute <- entity.attributes.values if attribute.typ.isInstanceOf[ManyToOneType])
-            yield
-              s"ALTER TABLE ${entity.table} ADD FOREIGN KEY (${attribute.column}) REFERENCES ${attribute.typ.asInstanceOf[ManyToOneType].entity.table};"
-
-    tables ++ foreignKeys.flatten
-  }
 
 }
