74d7fe938be2b8ea8668e5dfa123b82b17c68aad
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-04-11 18:27:40 -0400

    OQL-16 unit tests

--- shared/src/test/scala/com/vinctus/oql2/MinimalEmployeeDBTests.scala
+++ shared/src/test/scala/com/vinctus/oql2/MinimalEmployeeDBTests.scala
@@ -6,8 +6,56 @@ import org.scalatest.matchers.should.Matchers
 class MinimalEmployeeDBTests extends AnyFreeSpec with Matchers with MinimalEmployeeDB {
 
   "simplest self-join query" in {
-    test("employee { firstName manager { firstName } }") shouldBe
+    test("employee { * manager }") shouldBe
       """
+        |[
+        |  {
+        |    "id": 100,
+        |    "firstName": "Steven",
+        |    "lastName": "King",
+        |    "manager": null
+        |  },
+        |  {
+        |    "id": 101,
+        |    "firstName": "Neena",
+        |    "lastName": "Kochhar",
+        |    "manager": {
+        |      "id": 100,
+        |      "firstName": "Steven",
+        |      "lastName": "King"
+        |    }
+        |  },
+        |  {
+        |    "id": 102,
+        |    "firstName": "Lex",
+        |    "lastName": "De Haan",
+        |    "manager": {
+        |      "id": 100,
+        |      "firstName": "Steven",
+        |      "lastName": "King"
+        |    }
+        |  },
+        |  {
+        |    "id": 103,
+        |    "firstName": "Alexander",
+        |    "lastName": "Hunold",
+        |    "manager": {
+        |      "id": 102,
+        |      "firstName": "Lex",
+        |      "lastName": "De Haan"
+        |    }
+        |  },
+        |  {
+        |    "id": 104,
+        |    "firstName": "Bruce",
+        |    "lastName": "Ernst",
+        |    "manager": {
+        |      "id": 103,
+        |      "firstName": "Alexander",
+        |      "lastName": "Hunold"
+        |    }
+        |  }
+        |]
         |""".trim.stripMargin
   }
 
