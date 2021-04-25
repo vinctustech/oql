8a6f8c821e51f7f7dacec58b97ef8d5a52def5c4
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-04-25 11:08:51 -0400

    OQL-20 cont coding new (temporary) OQL parser

--- shared/src/main/scala/com/vinctus/oql2/OQLParser.scala
+++ shared/src/main/scala/com/vinctus/oql2/OQLParser.scala
@@ -1,6 +1,7 @@
 package com.vinctus.oql2
 
 import com.vinctus.oql2.StarOQLProject.label
+import org.checkerframework.checker.units.qual.s
 
 import scala.util.matching.Regex
 import scala.util.parsing.combinator.{PackratParsers, RegexParsers}
@@ -12,7 +13,7 @@ object OQLParser extends RegexParsers with PackratParsers {
 
   def kw(s: String): Regex = (s"(?i)$s\\b").r
 
-  lazy val command: PackratParser[OQLAST] = query | insert
+  lazy val command: PackratParser[OQLAST] = query //| insert
 
   lazy val query: PackratParser[OQLQuery] =
     entityName ~ project ~ opt("[" ~> logicalExpression <~ "]") ~ opt(group) ~ opt(order) ~ restrict ^^ {
@@ -20,25 +21,37 @@ object OQLParser extends RegexParsers with PackratParsers {
     }
 
   lazy val project: PackratParser[List[OQLProject]] = {
-  "{" ~ "*" ~ subtracts ~ attributeProjects ~ "}" ^^ {
-    case _ ~ _ ~ s ~ ps ~ _ => s.prepend(StarOQLProject).appendAll(ps)
+  "{" ~ "*" ~ subtracts ~ rep1(attributeProject) ~ "}" ^^ {
+    case _ ~ _ ~ s ~ ps ~ _ => s.prepended(StarOQLProject).appendedAll(ps)
   }
 
   lazy val subtracts: PackratParser[List[OQLProject]] = rep("-" ~> attributeName ^^ SubtractOQLProject)
 
     lazy val attributeProject: PackratParser[OQLProject] =
-      opt(label) ~ ident ~ "(" ~ argument
+      opt(label) ~ ident ~ "(" ~ argument ~ ")" ^^ {
+        case None ~ f ~ _ ~ StarOQLExpression ~ _ =>          ExpressionOQLProject(f,StarOQLExpression )
+        case None ~ f ~ _ ~ (a@AttributeOQLExpression(ids, _)) ~ _ =>          ExpressionOQLProject(Ident(ids.head.s, ids.head.pos),a )
+        case Some(l) ~ f ~ _ ~ StarOQLExpression ~ _ =>          ExpressionOQLProject(l,StarOQLExpression )
+        case Some(l) ~ f ~ _ ~ (a@AttributeOQLExpression(ids, _)) ~ _ =>          ExpressionOQLProject(l,a )
+  }
 
     lazy val label = ident <~ ":"
 
-    lazy val argument: PackratParser[OQLExpression] =
-      ident ^^
+    lazy val argument: PackratParser[OQLExpression] =      attributeExpression | starExpression
 
     lazy val entityName: PackratParser[Ident] = ident
 
     lazy val attributeName: PackratParser[Ident] = ident
 
-  lazy val expression: PackratParser[OQLExpression] = additive
+    lazy val attributeExpression: PackratParser[OQLExpression] = ident ^^ (id => AttributeOQLExpression(List(id)))
+
+    lazy val starExpression: PackratParser[OQLExpression] = "*" ^^^ StarOQLExpression
+
+    lazy val group
+
+    lazy val logicalExpression: PackratParser[OQLExpression] = additive
+
+    lazy val expression: PackratParser[OQLExpression] = additive
 
   lazy val additive: PackratParser[OQLExpression] =
     additive ~ ("+" | "-") ~ multiplicative ^^ {
@@ -53,9 +66,12 @@ object OQLParser extends RegexParsers with PackratParsers {
       primary
 
   lazy val primary: PackratParser[OQLExpression] =
-    "[0-9]+".r ^^ NumberExpression |
-      kw("exp") ~> primary ^^ ExpExpression |
-      ident ^^ VariableExpression
+    pos ~ integer ^^ {case p ~ n => IntegerOQLExpression(n, p)} |
+      pos ~ float ^^ {case p ~ n => FloatOQLExpression(n, p)}
+
+  lazy val float: PackratParser[Double] = """[0-9]*\.[0-9]+([eE][+-]?[0-9]+)?""".r ^^ (_.toDouble)
+
+  lazy val integer: PackratParser[Int] = "[0-9]+".r ^^ (_.toInt)
 
   lazy val ident: PackratParser[Ident] =
     pos ~ """[a-zA-Z_$][a-zA-Z0-9_$]*""".r ^^ {
@@ -63,7 +79,7 @@ object OQLParser extends RegexParsers with PackratParsers {
     }
 
   def parseQuery(input: String): OQLQuery =
-    parseAll(phrase(expression), new PackratReader(new CharSequenceReader(input))) match {
+    parseAll(phrase(query), new PackratReader(new CharSequenceReader(input))) match {
       case Success(result, _)     => result
       case NoSuccess(error, rest) => sys.error(s"$error: ${rest.pos}")
     }
