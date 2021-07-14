4dab4d130fe3c4acde403e11eb507d405ffc6d4e
Edward A Maxedon, Sr <edadma@gmail.com>
2021-07-14 16:38:10 -0400

    OQL-42 start coding bulkUpdate()

--- src/test/scala/com/vinctus/oql/Main.scala
+++ src/test/scala/com/vinctus/oql/Main.scala
@@ -1,3 +1,30 @@
+package com.vinctus.oql
+
+import com.vinctus.sjs_utils.Mappable
+import typings.node.global.console
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.async.Async.{async, await}
+
+import scala.scalajs.js
+import js.Dynamic.{global => g}
+
+object Main extends App {
+
+  g.require("source-map-support").install()
+
+  val db =
+    new OQL_NodePG(g.require("fs").readFileSync("test/int.dm").toString, "localhost", 5432, "postgres", "postgres", "docker", false, 1000, 5)
+
+  async {
+//    db.showQuery()
+//    await(db.entity("t").insert(Map("s" -> "asdf'zxcv")))
+    db.showQuery()
+    println(await(db.queryMany("""account""")))
+  }
+
+}
+
 //package com.vinctus.oql
 //
 //import com.vinctus.sjs_utils.Mappable
@@ -36,35 +63,37 @@
 //
 //}
 
-package com.vinctus.oql
-
-import com.vinctus.sjs_utils.Mappable
-import typings.node.global.console
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.scalajs.js
-import js.Dynamic.{global => g}
-import scala.util.{Failure, Success}
-import scala.async.Async.{async, await}
-
-object Main extends App {
-
-  g.require("source-map-support").install()
-
-  val dm: String =
-    """
-        |entity t {
-        | *id: int
-        |  n: int
-        |}
-        |""".stripMargin
-  val db = new OQL_NodePG_JS(dm, "localhost", 5432, "postgres", "postgres", "docker", false, 1000, 5)
-
-  db.showQuery()
-
-  async {
-    console.log(await(db.jsQueryMany("""t { average: avg(n) }""").toFuture))
-  }
+//package com.vinctus.oql
+//
+//import com.vinctus.sjs_utils.Mappable
+//import typings.node.global.console
+//
+//import scala.concurrent.ExecutionContext.Implicits.global
+//import scala.scalajs.js
+//import js.Dynamic.{global => g}
+//import scala.util.{Failure, Success}
+//import scala.async.Async.{async, await}
+//
+//object Main extends App {
+//
+//  g.require("source-map-support").install()
+//
+//  val dm: String =
+//    """
+//        |entity t {
+//        | *id: int
+//        |  n: int
+//        |}
+//        |""".stripMargin
+//  val db = new OQL_NodePG_JS(dm, "localhost", 5432, "postgres", "postgres", "docker", false, 1000, 5)
+//
+//  db.showQuery()
+//
+//  async {
+//    console.log(await(db.jsQueryMany("""t { average: avg(n) }""").toFuture))
+//  }
+//
+//}
 
 //  val dm: String =
 //    """
@@ -101,8 +130,8 @@ object Main extends App {
 //      }
 //    case Failure(exception) => exception.printStackTrace()
 //  }
-
-}
+//
+//}
 
 // todo: RDB insert without primary key value doesn't work
 // todo: error check for query type projects that are really datatype attributes: make sure there's no select, order, ...
