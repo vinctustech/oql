446d0f18e42497985f04cd6dbd10c0f96ab461ce
Edward A Maxedon Sr <edadma@gmail.com>
2022-02-12 12:07:01 -0500

    OQL-53 start coding support for JSON type data

---
+++ test/json.sql
@@ -0,0 +1,6 @@
+CREATE TABLE json (
+  id UUID,
+  data JSONB
+  );
+
+INSERT INTO json (id, data) VALUES ('843c15e8-1c6c-4148-b453-73a07cce1884', '[1,2,3]'), ('fbc3b34b-15bd-4c70-8e5a-6f268c45161a', '{"d": 4}');
