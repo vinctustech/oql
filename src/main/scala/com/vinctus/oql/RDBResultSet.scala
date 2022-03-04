ec12adb310b99b0da041b5735d9d86b11c9945c3
Edward A Maxedon Sr <edadma@gmail.com>
2022-03-04 18:45:22 -0500

    OQL-53 integrate in-memory database

--- src/main/scala/com/vinctus/oql/RDBResultSet.scala
+++ src/main/scala/com/vinctus/oql/RDBResultSet.scala
@@ -31,13 +31,14 @@ class RDBResultSet(rs: Iterator[Row]) extends OQLResultSet {
       case NumberValue(dal.IntType, value)    => value.intValue
       case NumberValue(dal.DoubleType, value) => value.doubleValue
       case NumberValue(dal.LongType, value)   => value.doubleValue // todo: js hack
-      case NumberValue(dal.BigDecType, value) => value.doubleValue // todo: js hack
-      case TextValue(s)                       => s
-      case UUIDValue(id)                      => id
-      case TimestampValue(t)                  => new js.Date(t.toISOString) // todo: js hack
-      case ArrayValue(data)                   => data map unpack
-      case ObjectValue(properties)            => properties map { case (k, v) => k -> unpack(v) } toMap
-      case NullValue()                        => null
+      case NumberValue(dal.BigDecType, value) =>
+        value.toString // todo: js hack (to retain decimals even if they are zero)
+      case TextValue(s)            => s
+      case UUIDValue(id)           => id
+      case TimestampValue(t)       => new js.Date(t.toISOString) // todo: js hack
+      case ArrayValue(data)        => data map unpack
+      case ObjectValue(properties) => properties map { case (k, v) => k -> unpack(v) } toMap
+      case NullValue()             => null
 
   def get(idx: Int): OQLResultSetValue = RDBResultSetValue(unpack(row.data(idx)))
 
--- src/test/scala/com/vinctus/oql/Main.scala
+++ src/test/scala/com/vinctus/oql/Main.scala
@@ -19,33 +19,104 @@ object Main extends App {
   types.setTypeParser(114.asInstanceOf[TypeId], (s: String) => s) // tell node-pg not to parse JSON
   types.setTypeParser(1114.asInstanceOf[TypeId], (s: String) => new js.Date(s"$s+00:00"))
 
-  val db = new OQL_RDB_ScalaJS(readFile("test/book.dm"))
+  val db = new OQL_RDB_ScalaJS(readFile("test/orders.dm"))
 
   rdb.executeSQL(
     """
-      |CREATE TABLE "author" (
-      |  "pk_author_id" BIGINT PRIMARY KEY,
+      |CREATE TABLE "agent" (
+      |  "agent_code" TEXT PRIMARY KEY,
+      |  "agent_name" TEXT,
+      |  "working_area" TEXT,
+      |  "commission" NUMERIC(15, 2),
+      |  "phone_no" TEXT
+      |);
+      |CREATE TABLE "customer" (
+      |  "cust_code" TEXT PRIMARY KEY,
       |  "name" TEXT
       |);
-      |CREATE TABLE "book" (
-      |  "pk_book_id" BIGINT PRIMARY KEY,
-      |  "title" TEXT,
-      |  "year" INTEGER,
-      |  "author_id" BIGINT
+      |CREATE TABLE "order" (
+      |  "ord_num" INTEGER PRIMARY KEY,
+      |  "ord_amount" NUMERIC(15, 2),
+      |  "advance_amount" NUMERIC(15, 2),
+      |  "ord_date" TEXT,
+      |  "cust_code" TEXT,
+      |  "agent_code" TEXT
       |);
-      |ALTER TABLE "book" ADD FOREIGN KEY ("author_id") REFERENCES "author";
-      |INSERT INTO "author" ("pk_author_id", "name") VALUES
-      |  (1, 'Robert Louis Stevenson'),
-      |  (2, 'Lewis Carroll'),
-      |  (3, 'Charles Dickens'),
-      |  (4, 'Mark Twain');
-      |INSERT INTO "book" ("pk_book_id", "title", "year", "author_id") VALUES
-      |  (1, 'Treasure Island', 1883, 1),
-      |  (2, E'Alice\'s Adventures in Wonderland', 1865, 2),
-      |  (3, 'Oliver Twist', 1838, 3),
-      |  (4, 'A Tale of Two Cities', 1859, 3),
-      |  (5, 'The Adventures of Tom Sawyer', 1876, 4),
-      |  (6, 'Adventures of Huckleberry Finn', 1884, 4);
+      |ALTER TABLE "order" ADD FOREIGN KEY ("cust_code") REFERENCES "customer";
+      |ALTER TABLE "order" ADD FOREIGN KEY ("agent_code") REFERENCES "agent";
+      |INSERT INTO "agent" ("agent_code", "agent_name", "working_area", "commission", "phone_no") VALUES
+      |  ('A007', 'Ramasundar', 'Bangalore', 0.15, '077-25814763'),
+      |  ('A003', 'Alex', 'London', 0.13, '075-12458969'),
+      |  ('A008', 'Alford', 'New York', 0.12, '044-25874365'),
+      |  ('A011', 'Ravi Kumar', 'Bangalore', 0.15, '077-45625874'),
+      |  ('A010', 'Santakumar', 'Chennai', 0.14, '007-22388644'),
+      |  ('A012', 'Lucida', 'San Jose', 0.12, '044-52981425'),
+      |  ('A005', 'Anderson', 'Brisban', 0.13, '045-21447739'),
+      |  ('A001', 'Subbarao', 'Bangalore', 0.14, '077-12346674'),
+      |  ('A002', 'Mukesh', 'Mumbai', 0.11, '029-12358964'),
+      |  ('A006', 'McDen', 'London', 0.15, '078-22255588'),
+      |  ('A004', 'Ivan', 'Torento', 0.15, '008-22544166'),
+      |  ('A009', 'Benjamin', 'Hampshair', 0.11, '008-22536178');
+      |INSERT INTO "customer" ("cust_code", "name") VALUES
+      |  ('C00002', 'C00002 asdf'),
+      |  ('C00003', 'C00003 asdf'),
+      |  ('C00023', 'C00023 asdf'),
+      |  ('C00007', 'C00007 asdf'),
+      |  ('C00008', 'C00008 asdf'),
+      |  ('C00025', 'C00025 asdf'),
+      |  ('C00004', 'C00004 asdf'),
+      |  ('C00021', 'C00021 asdf'),
+      |  ('C00011', 'C00011 asdf'),
+      |  ('C00001', 'C00001 asdf'),
+      |  ('C00020', 'C00020 asdf'),
+      |  ('C00006', 'C00006 asdf'),
+      |  ('C00005', 'C00005 asdf'),
+      |  ('C00018', 'C00018 asdf'),
+      |  ('C00014', 'C00014 asdf'),
+      |  ('C00022', 'C00022 asdf'),
+      |  ('C00009', 'C00009 asdf'),
+      |  ('C00010', 'C00010 asdf'),
+      |  ('C00017', 'C00017 asdf'),
+      |  ('C00024', 'C00024 asdf'),
+      |  ('C00015', 'C00015 asdf'),
+      |  ('C00012', 'C00012 asdf'),
+      |  ('C00019', 'C00019 asdf'),
+      |  ('C00016', 'C00016 asdf');
+      |INSERT INTO "order" ("ord_num", "ord_amount", "advance_amount", "ord_date", "cust_code", "agent_code") VALUES
+      |  (200114, 3500, 2000, '15-AUG-08', 'C00002', 'A008'),
+      |  (200122, 2500, 400, '16-SEP-08', 'C00003', 'A004'),
+      |  (200118, 500, 100, '20-JUL-08', 'C00023', 'A006'),
+      |  (200119, 4000, 700, '16-SEP-08', 'C00007', 'A010'),
+      |  (200121, 1500, 600, '23-SEP-08', 'C00008', 'A004'),
+      |  (200130, 2500, 400, '30-JUL-08', 'C00025', 'A011'),
+      |  (200134, 4200, 1800, '25-SEP-08', 'C00004', 'A005'),
+      |  (200108, 4000, 600, '15-FEB-08', 'C00008', 'A004'),
+      |  (200103, 1500, 700, '15-MAY-08', 'C00021', 'A005'),
+      |  (200105, 2500, 500, '18-JUL-08', 'C00025', 'A011'),
+      |  (200109, 3500, 800, '30-JUL-08', 'C00011', 'A010'),
+      |  (200101, 3000, 1000, '15-JUL-08', 'C00001', 'A008'),
+      |  (200111, 1000, 300, '10-JUL-08', 'C00020', 'A008'),
+      |  (200104, 1500, 500, '13-MAR-08', 'C00006', 'A004'),
+      |  (200106, 2500, 700, '20-APR-08', 'C00005', 'A002'),
+      |  (200125, 2000, 600, '10-OCT-08', 'C00018', 'A005'),
+      |  (200117, 800, 200, '20-OCT-08', 'C00014', 'A001'),
+      |  (200123, 500, 100, '16-SEP-08', 'C00022', 'A002'),
+      |  (200120, 500, 100, '20-JUL-08', 'C00009', 'A002'),
+      |  (200116, 500, 100, '13-JUL-08', 'C00010', 'A009'),
+      |  (200124, 500, 100, '20-JUN-08', 'C00017', 'A007'),
+      |  (200126, 500, 100, '24-JUN-08', 'C00022', 'A002'),
+      |  (200129, 2500, 500, '20-JUL-08', 'C00024', 'A006'),
+      |  (200127, 2500, 400, '20-JUL-08', 'C00015', 'A003'),
+      |  (200128, 3500, 1500, '20-JUL-08', 'C00009', 'A002'),
+      |  (200135, 2000, 800, '16-SEP-08', 'C00007', 'A010'),
+      |  (200131, 900, 150, '26-AUG-08', 'C00012', 'A012'),
+      |  (200133, 1200, 400, '29-JUN-08', 'C00009', 'A002'),
+      |  (200100, 1000, 600, '08-JAN-08', 'C00015', 'A003'),
+      |  (200110, 3000, 500, '15-APR-08', 'C00019', 'A010'),
+      |  (200107, 4500, 900, '30-AUG-08', 'C00007', 'A010'),
+      |  (200112, 2000, 400, '30-MAY-08', 'C00016', 'A007'),
+      |  (200113, 4000, 600, '10-JUN-08', 'C00022', 'A002'),
+      |  (200102, 2000, 300, '25-MAY-08', 'C00012', 'A012');
       |""".stripMargin
   )(
     db.connect
@@ -53,14 +124,8 @@ object Main extends App {
       .db
   )
 
-  (for
-    //    _ <- db.create()
-//    _ <- db.entity("author").insert(Map("name" -> "Robert Louis Stevenson"))
-//    _ <- db.entity("book").insert(Map("title" -> "Treasure Island", "year" -> 1883, "author" -> 1))
-//    _ <- db.entity("author").insert(Map("name" -> "Lewis Carroll"))
-//    _ <- db.entity("book").insert(Map("title" -> "Alice's Adventures in Wonderland", "year" -> 1865, "author" -> 2))
-    r <- { db.showQuery(); db.queryMany("author { name books: books {count(*)} }") }
-  yield r)
+  db.showQuery()
+  db.queryMany("agent { * -phone_no orders { sum(ord_amount) } } [working_area = 'Bangalore'] <agent_code>")
     .onComplete {
       case Failure(exception) => exception.printStackTrace()
       case Success(value)     => println(value)
